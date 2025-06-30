3b0bb8f6373bbda46d5c1c7c0699a7afa1c69cf5
kamillylayza <layza.amorim@escola.pr.gov.br>
2025-06-30 02:12:28 +0000

    aula 6 do alura

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
-  botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
   const texto = document.querySelector("#entrada-de-texto").value;
 
   const campoResultado = document.querySelector("#resultado-palavrachave");
+
   const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavrasChave.join(", ");
+  campoResultado.textContent = palavrasChave.join(", ");
 }
 
-  function processaTexto(texto) {
+function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
+  const frequencias = contaFrequencias(palavras);
+
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
   let frequencias = {};
 
   for (let i of palavras) {
@@ -25,7 +39,5 @@ function mostraPalavrasChave() {
     }
   }
 
-  console.log(frequencias);
-
-  return palavras;
+  return frequencias;
 }
